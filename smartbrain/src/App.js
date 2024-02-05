03d17a69f21dedec422936fda0930b22dc04ee4b
unknown <bijoordzen@gmail.com>
2024-02-05 16:17:53 +0530

    first commit

---
+++ smartbrain/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import LinkForm from './components/imagelinkform/ImageLinkform';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Logo/>
+      <LinkForm/>
+      {/*
+     
+  <FaceRecognition/>*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ smartbrain/src/components/imagelinkform/ImageLinkform.js
@@ -0,0 +1,16 @@
+import React from 'react';
+const LinkForm = () => {
+return (
+        <div>
+            <p>
+               {'Smarty will Detect faces from Images'} 
+            </p>
+            <div>
+                <input type='text'></input>
+                <button>Submit</button>
+            </div>
+                
+        </div>
+    );
+}
+export default LinkForm;
